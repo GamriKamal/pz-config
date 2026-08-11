328539c82775b1c910dacc2fa1d487faa0cd7669
mr_irmag <mrirmag.pr@gmail.com>
2026-08-11 16:57:29 +0500

    feat(config): new options for b42

--- servertest_spawnregions.lua
+++ servertest_spawnregions.lua
@@ -1,14 +1,12 @@
 function SpawnRegions()
-	return {
-        -- Добавленные города из мода
-        { name = "Brandenburg, KY", file = "media/maps/Brandenburg, KY/spawnpoints.lua" },
-        { name = "Ekron, KY", file = "media/maps/Ekron, KY/spawnpoints.lua" },
-        { name = "Irvington, KY", file = "media/maps/Irvington, KY/spawnpoints.lua" },
-        
-        -- Стандартные города
+    return {
+        { name = "map_distanciado", file = "media/maps/map_distanciado/spawnpoints.lua" },
         { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
         { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
         { name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
         { name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+        { name = "March Ridge, KY", file = "media/maps/March Ridge, KY/spawnpoints.lua" },
+        { name = "Louisville, KY", file = "media/maps/Louisville, KY/spawnpoints.lua" },
+        { name = "Valley Station, KY", file = "media/maps/Valley Station, KY/spawnpoints.lua" },
     }
 end
